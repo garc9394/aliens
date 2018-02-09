86155031f4e260112d093b20acba16aa9f413819
Gar Cheuy <garcheuy@yahoo.com>
2018-02-09 09:00:04 -0800

    commit

--- index.js
+++ index.js
@@ -10,20 +10,15 @@ $datetimeInput.addEventListener('keypress', function (event) {
 });
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// Set filteredAddresses to addressData initially
 var filteredData = dataSet;
 
-// renderTable renders the filteredAddresses to the tbody
 function renderTable() {
   $tbody.innerHTML = "";
   for (var i = 0; i < filteredData.length; i++) {
-    // Get get the current address object and its fields
     var data = filteredData[i];
     var fields = Object.keys(data);
-    // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
       var field = fields[j];
       var $cell = $row.insertCell(j);
       $cell.innerText = data[field];
@@ -32,18 +27,14 @@ function renderTable() {
 }
 
 function handleSearchButtonClick() {
-  // Format the user's search by removing leading and trailing whitespace, lowercase the string
   var filterDatetime = $datetimeInput.value.trim().toLowerCase();
 
-  // Set filteredAddresses to an array of all addresses whose "state" matches the filter
   filteredData = dataSet.filter(function(data) {
     var dataDatetime = data.datetime.toLowerCase();
 
-    // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
     return dataDatetime === filterDatetime;
   });
   renderTable();
 }
 
-// Render the table for the first time on page load
 renderTable();
