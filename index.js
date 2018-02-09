65c0f5e3998c76dd3c49e3d9b9d08123edc086ab
garc9394 <garcheuy@yahoo.com>
2018-02-08 18:32:43 -0800

    commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function renderTable() {
   $tbody.innerHTML = "";
   for (var i = 0; i < filteredData.length; i++) {
     // Get get the current address object and its fields
-    var address = filteredData[i];
+    var data = filteredData[i];
     var fields = Object.keys(data);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
@@ -32,7 +32,7 @@ function handleSearchButtonClick() {
   var filterDatetime = $datetimeInput.value.trim().toLowerCase();
 
   // Set filteredAddresses to an array of all addresses whose "state" matches the filter
-  filteredDatetime = dataSet.filter(function(data) {
+  filteredData = dataSet.filter(function(data) {
     var dataDatetime = data.datetime.toLowerCase();
 
     // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
